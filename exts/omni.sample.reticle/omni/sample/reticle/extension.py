4a72b63e8af9161979f61aa8406669823fbc7e44
mati-nvidia <mcodesal@nvidia.com>
2022-06-22 02:45:24 -0400

    v1.1.0 - Fixed startup exception. - Renamed reticle.py to views.py

--- exts/omni.sample.reticle/omni/sample/reticle/extension.py
+++ exts/omni.sample.reticle/omni/sample/reticle/extension.py
@@ -1,64 +1,28 @@
-import traceback
-
+import carb
 import omni.ext
-import omni.ui as ui
+from omni.kit.viewport.utility import get_active_viewport_window
 
+from . import constants
 from .models import ReticleModel
-from .reticle import ReticleOverlay
-
-
-class CleanupError(Exception):
-    pass
+from .views import ReticleOverlay
 
 
 class SampleViewportReticleExtension(omni.ext.IExt):
 
     def on_startup(self, ext_id):
         print("[omni.sample.reticle] SampleViewportReticleExtension startup")
-        windows = ui.Workspace.get_windows()
-        for window_handle in windows:
-            if window_handle.title.startswith("Viewport"):
-                window = ui.Window(window_handle.title)
-                SampleViewportReticleExtension.create_new_reticle_overlay(window)
-        # ui.Workspace.set_window_created_callback(SampleViewportReticleExtension.on_window_created)
 
-    @staticmethod
-    def create_new_reticle_overlay(vp_win):
-        reticle_model = ReticleModel()
-        reticle = ReticleOverlay(reticle_model, vp_win)
-        reticle.build_viewport_overlay()
+        # Reticle should ideally be used with "Fill Viewport" turned off.
+        settings = carb.settings.get_settings()
+        settings.set(constants.SETTING_RESOLUTION_FILL, False)
 
-    @staticmethod
-    def on_window_created(win_handle):
-        """Add a new ReticleOverlay whenever a new viewport window is created.
-
-        Args:
-            win_handle (WindowHandle): The window that was created.
-        """
-        if win_handle.title.startswith("Viewport"):
-            window = ui.Window(win_handle.title)
-            SampleViewportReticleExtension.create_new_reticle_overlay(window)
+        viewport_window = get_active_viewport_window()
+        if viewport_window is not None:
+            reticle_model = ReticleModel()
+            self.reticle = ReticleOverlay(reticle_model, viewport_window)
+            self.reticle.build_viewport_overlay()
 
     def on_shutdown(self):
-        """ Executed when the extension is disabled.
-
-        TODO:
-            * Overlay widgets are not removed on shutdown and users can still click on
-            the Reticle button and produce an error.
-            * I think object references are still being held on for callbacks. I need make sure everything is destroyed.
-
-        Raises:
-            CleanupError: Error occurred trying to destroy ReticleOverlay instances.
-        """
+        """ Executed when the extension is disabled."""
         print("[omni.sample.reticle] SampleViewportReticleExtension shutdown")
-        exception_raised = False
-        # Need to explicitly destroy the ViewportWidgets otherwise they'll stick around and consume
-        for instance in ReticleOverlay.get_instances():
-            try:
-                instance.destroy()
-            except Exception:
-                traceback.print_exc()
-                exception_raised = True
-
-        if exception_raised:
-            raise CleanupError("One more more exceptions were raised when trying to destroy ReticleOverlay instances.")
+        self.reticle.destroy()
